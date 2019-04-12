d4fd3f854cf7a7da957181bc090e32813f47934e
Ilya Oleshko <iliaoleshko@yandex.ru>
2019-04-12 23:04:43 +0300

    Create CRUDForm.Designer.cs

---
+++ OOP_CRUD/CRUDForm.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace OOP_CRUD
+{
+    partial class CRUDForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonCreate = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonEdit = new System.Windows.Forms.Button();
+            this.comboBoxTypes = new System.Windows.Forms.ComboBox();
+            this.itemsListView = new System.Windows.Forms.ListView();
+            this.checkBoxFilter = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // buttonCreate
+            // 
+            this.buttonCreate.Location = new System.Drawing.Point(313, 6);
+            this.buttonCreate.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonCreate.Name = "buttonCreate";
+            this.buttonCreate.Size = new System.Drawing.Size(157, 40);
+            this.buttonCreate.TabIndex = 2;
+            this.buttonCreate.Text = "Create";
+            this.buttonCreate.UseVisualStyleBackColor = true;
+            this.buttonCreate.Click += new System.EventHandler(this.buttonCreate_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(312, 50);
+            this.buttonDelete.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(157, 40);
+            this.buttonDelete.TabIndex = 3;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonEdit
+            // 
+            this.buttonEdit.Location = new System.Drawing.Point(312, 94);
+            this.buttonEdit.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonEdit.Name = "buttonEdit";
+            this.buttonEdit.Size = new System.Drawing.Size(157, 40);
+            this.buttonEdit.TabIndex = 4;
+            this.buttonEdit.Text = "Edit";
+            this.buttonEdit.UseVisualStyleBackColor = true;
+            this.buttonEdit.Click += new System.EventHandler(this.buttonEdit_Click);
+            // 
+            // comboBoxTypes
+            // 
+            this.comboBoxTypes.FormattingEnabled = true;
+            this.comboBoxTypes.Location = new System.Drawing.Point(312, 139);
+            this.comboBoxTypes.Name = "comboBoxTypes";
+            this.comboBoxTypes.Size = new System.Drawing.Size(156, 21);
+            this.comboBoxTypes.TabIndex = 5;
+            this.comboBoxTypes.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // itemsListView
+            // 
+            this.itemsListView.FullRowSelect = true;
+            this.itemsListView.Location = new System.Drawing.Point(13, 6);
+            this.itemsListView.MultiSelect = false;
+            this.itemsListView.Name = "itemsListView";
+            this.itemsListView.Size = new System.Drawing.Size(295, 206);
+            this.itemsListView.TabIndex = 6;
+            this.itemsListView.UseCompatibleStateImageBehavior = false;
+            this.itemsListView.View = System.Windows.Forms.View.Details;
+            // 
+            // checkBoxFilter
+            // 
+            this.checkBoxFilter.AutoSize = true;
+            this.checkBoxFilter.Location = new System.Drawing.Point(314, 166);
+            this.checkBoxFilter.Name = "checkBoxFilter";
+            this.checkBoxFilter.Size = new System.Drawing.Size(130, 30);
+            this.checkBoxFilter.TabIndex = 7;
+            this.checkBoxFilter.Text = "Только экземпляры\r\n выбранного класса";
+            this.checkBoxFilter.UseVisualStyleBackColor = true;
+            this.checkBoxFilter.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // CRUDForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(481, 223);
+            this.Controls.Add(this.checkBoxFilter);
+            this.Controls.Add(this.itemsListView);
+            this.Controls.Add(this.comboBoxTypes);
+            this.Controls.Add(this.buttonEdit);
+            this.Controls.Add(this.buttonCreate);
+            this.Controls.Add(this.buttonDelete);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "CRUDForm";
+            this.Text = "Weapon CRUD";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonCreate;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonEdit;
+        private System.Windows.Forms.ComboBox comboBoxTypes;
+        private System.Windows.Forms.ListView itemsListView;
+        private System.Windows.Forms.CheckBox checkBoxFilter;
+    }
+}
+
