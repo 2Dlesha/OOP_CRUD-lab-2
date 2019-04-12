bb6da09d788aba16dddf9dcea6f2aa944aa3d0f0
Ilya Oleshko <iliaoleshko@yandex.ru>
2019-04-13 00:06:27 +0300

    Update CRUDForm.cs

--- OOP_CRUD/CRUDForm.cs
+++ OOP_CRUD/CRUDForm.cs
@@ -13,97 +13,80 @@ namespace OOP_CRUD
 {
     public partial class CRUDForm : Form
     {
-        public Form frm;
+        public CRUDHelper CRUDAssistant = null;
+        public Form editForm = null;
         public List<object> itemList = new List<object>();
         public List<object> activeItemList = new List<object>();
 
         public List<Type> itemCreator = new List<Type>();
 
-        public CRUDForm()
+        public CRUDForm(List<Type> availibleTypes, CRUDHelper CRUDHelper)
         {
             InitializeComponent();
-        }
-
-        public CRUDForm(List<Type> availibleTypes)
-        {
-            InitializeComponent();
+            CRUDAssistant = CRUDHelper;
+            CRUDAssistant.ItemsInit(itemList);
             itemCreator = availibleTypes;
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void CRUDForm_Load(object sender, EventArgs e)
         {
-            CRUDHelper.ItemsInit(itemList);
             activeItemList = itemList;
 
             foreach (var item in itemCreator)
             {
-                string typeString = "";
+                string typeString = item.Name; 
+
                 if (item.GetCustomAttributes(typeof(DescriptionAttribute), false).FirstOrDefault() is DescriptionAttribute descriptionAttribute)
                     typeString = descriptionAttribute.Description;
-                else
-                    typeString = item.Name;
 
                 comboBoxTypes.Items.Add(typeString);
             }
 
             comboBoxTypes.SelectedIndex = 0;
             comboBoxTypes.DropDownStyle = ComboBoxStyle.DropDownList;
-
-            CRUDHelper.ListRedraw(itemsListView, activeItemList, "name");
             itemsListView.View = View.Details;
+
+            CRUDAssistant.ListRedraw(itemsListView, activeItemList);
         }
 
         private void buttonCreate_Click(object sender, EventArgs e)
         {
-            ListView listview = itemsListView;
-
             ConstructorInfo itemConstructor = itemCreator[comboBoxTypes.SelectedIndex].GetConstructor(new Type[] { });
             object newitem = itemConstructor.Invoke(new object[] { });
             itemList.Add(newitem);
 
             activeItemList = GetActiveList(itemList, checkBoxFilter.Checked, itemCreator[comboBoxTypes.SelectedIndex]);
-            CRUDHelper.ListRedraw(itemsListView, activeItemList, "name");
+            CRUDAssistant.ListRedraw(itemsListView, activeItemList);
 
-            ListViewItem lvi = listview.Items[(activeItemList.Count - 1)];
-            if (lvi != null)
+            ListViewItem listViewItem = itemsListView.Items[(activeItemList.Count - 1)];
+            if (listViewItem != null)
             {
-                listview.Focus();
-                lvi.Selected = true;
+                itemsListView.Focus();
+                listViewItem.Selected = true;
                 buttonEdit.PerformClick();
             }
         }
 
         private void buttonDelete_Click(object sender, EventArgs e)
         {
-            ListView listview = GetActiveList();
-            if (listview == null)
-                return;
-
             object item = GetFocusItem();
             if (item != null)
             {
-                CRUDHelper.DeleteItem(item, itemList);
+                CRUDAssistant.DeleteItem(item, itemList);
                 activeItemList = GetActiveList(itemList, checkBoxFilter.Checked, itemCreator[comboBoxTypes.SelectedIndex]);
-                CRUDHelper.ListRedraw(itemsListView, activeItemList, "name");
+                CRUDAssistant.ListRedraw(itemsListView, activeItemList);
             }
         }
 
         private void buttonEdit_Click(object sender, EventArgs e)
         {
-            ListView listview = GetActiveList();
-            if (listview == null)
-                return ;
-
             object item = GetFocusItem();
             if (item != null)
             {
-                FieldInfo[] fields = item.GetType().GetFields(); 
-
-                frm = CRUDHelper.CreateForm(item, itemList,SaveControls);
-                frm.ShowDialog();
-                frm.Dispose();
-
-                CRUDHelper.ListRedraw(itemsListView, activeItemList, "name");
+                editForm = CRUDAssistant.CreateForm(item, itemList,SaveControls);
+                editForm.ShowDialog();
+                editForm.Dispose();
+                CRUDAssistant.ListRedraw(itemsListView, activeItemList);
             }
         }
 
@@ -111,11 +94,8 @@ namespace OOP_CRUD
         public void SaveControls(object sender, EventArgs e)
         {
             object item = GetFocusItem();
-            if (item == null)
-            {
-                return;
-            }
-            CRUDHelper.SaveControlsToItems(item, itemList, frm);     
+            if (item != null)
+                CRUDAssistant.SaveControlsToItems(item, itemList, editForm);
         }
 
         public object GetFocusItem()
@@ -123,48 +103,30 @@ namespace OOP_CRUD
             object item = null;
             activeItemList = GetActiveList(itemList, checkBoxFilter.Checked, itemCreator[comboBoxTypes.SelectedIndex]);
 
-            if ((itemsListView!=null) &&(itemsListView.SelectedIndices.Count != 0))
-            {
+            if (itemsListView.SelectedIndices.Count != 0)
                 item = activeItemList[itemsListView.SelectedIndices[0]];
-            }
 
             return item;
         }
 
-        public ListView GetActiveList()
-        {
-            foreach (ListView listview in this.Controls.OfType<ListView>())
-            {
-                if (listview.SelectedIndices.Count != 0)
-                {
-                    return listview;
-                }
-            }
-            return null;
-        }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
             activeItemList = GetActiveList(itemList, checkBoxFilter.Checked, itemCreator[comboBoxTypes.SelectedIndex]);
-            CRUDHelper.ListRedraw(itemsListView, activeItemList, "name");
+            CRUDAssistant.ListRedraw(itemsListView, activeItemList);
         }
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
             activeItemList = GetActiveList(itemList, checkBoxFilter.Checked, itemCreator[comboBoxTypes.SelectedIndex]);
-            CRUDHelper.ListRedraw(itemsListView, activeItemList, "name");
+            CRUDAssistant.ListRedraw(itemsListView, activeItemList);
         }
 
         public  List<object> GetActiveList(List<object> items, bool specificClass, Type classType)
         {
-            if ( specificClass && (classType != null) )
-            {
-                return items.Where(item => (item.GetType() == classType)).ToList();
-            }
-            else
-            {
-                return items;
-            }
+            return (specificClass && (classType != null)) 
+                ?  items.Where(item => (item.GetType() == classType)).ToList() 
+                :  items;
         }
     }
 }
