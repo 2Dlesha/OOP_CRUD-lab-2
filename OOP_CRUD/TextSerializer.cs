acbf8e2b06ba0ee8b8f23602999aca805a616ddd
Ilya Oleshko <iliaoleshko@yandex.ru>
2019-04-18 02:31:23 +0300

    Create TextSerializer.cs

---
+++ OOP_CRUD/TextSerializer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace OOP_CRUD
+{
+    class TextSerializer : ISerializer
+    {
+
+        public string FilePath { get; set; }
+
+        public TextSerializer()
+        {
+            FilePath = "CRUD.data";
+        }
+
+
+        public void Serialize(Object itemList)
+        { 
+
+
+            string objectInfo = String.Empty;
+            //MessageBox.Show(objectInfo);
+            using (StreamWriter streamWriter= new StreamWriter(FilePath))
+            {
+                streamWriter.Write(objectInfo);
+            }
+        }
+
+        public Object Deserialize(Object itemList)
+        {
+
+            
+
+            string objectInfo = String.Empty;
+            using (StreamReader streamReader = new StreamReader(FilePath))
+            {
+                objectInfo = streamReader.ReadToEnd();
+            }
+            object deserializedObject = new List<Object>();
+            return (List<Object>)deserializedObject;
+        }
+    }
+
+
+
+
+
+}
